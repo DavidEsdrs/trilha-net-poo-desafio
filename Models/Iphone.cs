11f4e2bcb11641d3754280ab1640f4bc608398e1
DavidEsdrs <carpina1983@gmail.com>
2024-05-15 19:05:20 -0300

    feat: implement smartphone class

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei) : base(numero, modelo, imei) { }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando aplication ios...");
+        }
     }
 }
